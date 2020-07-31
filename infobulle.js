4704ddb549637eeae88d17a9b8b78ede0fa42ea8
Jordan Fumiere <fumiere.jordan@outlook.fr>
2020-07-31 19:57:16 +0200

    Script permettant de modifier l'infobulle des éléments du profil utilisateur

---
+++ infobulle.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  
+	$('#seventh-prof-zero table a img[src$="https://zupimages.net/up/20/30/cxa6.png"]').parent().attr('title','Accéder à la fiche de liens');
+	$('#seventh-prof-zero table a img[src$="https://zupimages.net/up/20/30/iz6w.png"]').parent().attr('title','Accéder à la fiche de présentation');
+	$('#seventh-prof-zero table a img[src$="https://zupimages.net/up/20/30/dpsa.png"]').parent().attr('title','Accéder au carnet d\'aventures');
+	$('#seventh-prof-zero table a img[src$="https://zupimages.net/up/20/30/8reb.png"]').parent().attr('title','Accéder à l\'historique des promotions');
+  
+});
